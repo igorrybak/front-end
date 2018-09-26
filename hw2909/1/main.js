1ec5092be549adfb8ad182888c0b6ec196704dae
Igor <hostel@ukr.net>
2018-09-26 21:20:32 +0300

    First hw done

---
+++ hw2909/1/main.js
@@ -0,0 +1,17 @@
+// 1) Даны четыре действительных числа: x1, y1, x2, y2. Напишите функцию distance(x1, y1, x2, y2),
+// вычисляющую расстояние между точкой (x1,y1) и (x2,y2). Считайте четыре действительных числа и
+// выведите результат работы этой функции.
+
+alert("Введите последовательно четыре числа:");
+
+var num1 = prompt();
+var num2 = prompt();
+var num3 = prompt();
+var num4 = prompt();
+
+function calcDistance(n1, n2, n3, n4) {
+  let distance = Math.sqrt(Math.pow((n1 - n3), 2) + Math.pow((n2 - n4), 2));
+  return (Math.round(distance * 100) / 100);
+};
+
+alert("Расстояние между точками: " + calcDistance(num1, num2, num3, num4));
