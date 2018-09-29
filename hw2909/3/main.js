322b862a3d964a5ab6111cbc213790bdeb4576fa
Igor <hostel@ukr.net>
2018-09-29 08:42:24 +0300

    Third hw done

--- hw2909/3/main.js
+++ hw2909/3/main.js
@@ -2,3 +2,14 @@
 // i-я ступенька состоит из чисел от 1 до i без пробелов.
 
 
+const naturNum = prompt("Введите натуральное число от 1 до 9:");
+
+function drawStairs(num) {
+    let str = "";
+    for (let i = 1; i <= num; i++) {
+        str += i.toString();
+        console.log(str);
+    };
+};
+
+console.log(drawStairs(naturNum));
