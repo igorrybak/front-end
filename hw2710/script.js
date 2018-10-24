7c69f4f6ad7441ae62f4bdb4587897cd39788539
Igor <hostel@ukr.net>
2018-10-24 20:47:57 +0300

    Three lessons done

---
+++ hw2710/script.js
@@ -0,0 +1,25 @@
+// В прикрипленном файле найти и выполнить
+
+// 1) Найти все div и вывести их количество
+const allDivs = document.getElementsByTagName("div");
+console.log(allDivs.length);
+
+// 2) Все дивы без текста заполнить строкой текста
+const someText = "Lorem ispum";
+for (const key in allDivs) {
+    if (!allDivs[key].innerHTML) {
+        allDivs[key].innerHTML = someText;
+    }
+}
+
+// 3) Найти div с классом red и убедиться что это действительно div
+const redDiv = document.getElementsByClassName("red")[0] instanceof HTMLDivElement ?
+    document.getElementsByClassName("red")[0] : undefined;
+
+// 4) Внутри div создать p с текстом и img, в картинку добавить класс и ссылку на картинку
+// 5) Удалить предпоследний элемент в body
+// 6) Получить кол-во узлов в body
+// 7) Удалить все узлы типа text
+// 8) Создать узел типа комментарий и вставить его в начало body 
+// 9) Достать текст из комментария
+// 10) Удалить все узлы в body
