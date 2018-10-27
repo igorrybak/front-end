6283fe89756224cf25e49eef1f5c56672808777e
Igor <hostel@ukr.net>
2018-10-27 21:18:20 +0300

    HW done

--- hw2710/script.js
+++ hw2710/script.js
@@ -18,17 +18,39 @@ const redDiv = document.getElementsByClassName("red")[0] instanceof HTMLDivEleme
 
 // 4) Внутри div создать p с текстом и img, в картинку добавить класс и ссылку на картинку
 const p = document.createElement("p");
-p.textContent = someText;
-
 const img = document.createElement("img");
-img.add
-p.appendChild(img);
+
+p.textContent = someText;
+img.src = "https://c.tcdn.co/53c/d19/53cd1968-1bb2-11e8-9132-0007cb0b8645/channel256.png";
+img.classList.add("img");
 
 redDiv.appendChild(p);
+redDiv.appendChild(img);
 
 // 5) Удалить предпоследний элемент в body
+const allBodyNodes = document.body.getElementsByTagName('*');
+document.body.removeChild(allBodyNodes[allBodyNodes.length - 3]);
+
 // 6) Получить кол-во узлов в body
+const nodesCount = document.body.childNodes.length;
+
 // 7) Удалить все узлы типа text
+const allNodes = document.body.childNodes;
+
+for (const key in allNodes) {
+    if (allNodes[key].nodeName == "#text") {
+        allNodes[key].remove();
+    };
+};
+
 // 8) Создать узел типа комментарий и вставить его в начало body 
+const commentNode = document.createComment("My comment");
+document.body.insertBefore(commentNode, allBodyNodes[0]);
+
 // 9) Достать текст из комментария
+const foundedComment = document.body.childNodes[0].nodeValue;
+
 // 10) Удалить все узлы в body
+allNodes.forEach(el => {
+    document.body.removeChild(el);
+});
